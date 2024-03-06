92895fc13db1a87e1bf738c5f1b414e21e991248
SheveleffRoman <@>
2024-03-07 02:42:26 +0400

    feat: add port from env, global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenv.config();
+
+export const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
 }
 bootstrap();
