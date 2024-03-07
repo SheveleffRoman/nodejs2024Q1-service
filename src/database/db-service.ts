d8b970b61aa7d5bee3304adf816a5a241c2269f9
SheveleffRoman <@>
2024-03-08 01:08:19 +0400

    feat: add artist album fav db methods

--- src/database/db-service.ts
+++ src/database/db-service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { IDatabase } from '../interfaces';
 import { User } from 'src/user/entities/user.entity';
 import { Track } from 'src/track/entities/track.entity';
-import { UpdateTrackDto } from 'src/track/dto/update-track.dto';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Album } from 'src/album/entities/album.entity';
+import { Fav } from 'src/favs/entities/fav.entity';
 
 /** random uuid v4 for fake data
-644e05b5-5904-4345-b29a-052a1df44a82
-56bb26d5-d2b0-4670-aa89-9dfe8411af2a
+
 487b0e19-c9a9-4259-9a64-980fec56ef68
 8fcc09e8-5df5-40bc-9497-9299952a3da8
 89e44784-deff-4492-a3c7-f7a433089a01
@@ -35,17 +36,30 @@ export class DbService {
         updatedAt: 345435,
       },
     ],
-    artists: [],
+    artists: [
+      {
+        id: '644e05b5-5904-4345-b29a-052a1df44a82',
+        name: 'Dua Lipa',
+        grammy: true,
+      },
+    ],
     tracks: [
       {
         id: '3c7db318-d6de-4ab9-b6cb-66377fb88977',
         name: 'training season',
-        artistId: '4ff1b43b-ba4b-4f36-90a9-ffcaa732a3b7',
-        albumId: null,
+        artistId: '644e05b5-5904-4345-b29a-052a1df44a82',
+        albumId: '56bb26d5-d2b0-4670-aa89-9dfe8411af2a',
         duration: 3,
       },
     ],
-    albums: [],
+    albums: [
+      {
+        id: '56bb26d5-d2b0-4670-aa89-9dfe8411af2a',
+        name: 'One',
+        year: 2024,
+        artistId: '644e05b5-5904-4345-b29a-052a1df44a82',
+      },
+    ],
     favorites: {
       artists: [],
       albums: [],
@@ -101,5 +115,107 @@ export class DbService {
 
   deleteTrack(id: string) {
     return (this.db.tracks = this.db.tracks.filter((track) => track.id !== id));
+  }
+
+  //artist
+
+  getAllArtists(): Artist[] {
+    return this.db.artists;
+  }
+
+  getArtistById(id: string): Artist {
+    return this.db.artists.find((artist) => artist.id === id);
+  }
+
+  addArtist(dto: Artist) {
+    this.db.artists.push(dto);
+    return dto;
+  }
+
+  updateArtist(dto: Partial<Artist>): void {
+    this.db.artists = this.db.artists.map((artist) =>
+      artist.id === dto.id ? { ...artist, ...dto } : artist,
+    );
+  }
+
+  deleteArtist(id: string) {
+    this.db.artists = this.db.artists.filter((artist) => artist.id !== id);
+  }
+
+  //album
+
+  getAllAlbums() {
+    return this.db.albums;
+  }
+
+  getAlbumById(id: string): Album {
+    return this.db.albums.find((album) => album.id === id);
+  }
+
+  addAlbum(dto: Album) {
+    this.db.albums.push(dto);
+    return dto;
+  }
+
+  updateAlbum(dto: Partial<Album>) {
+    this.db.albums = this.db.albums.map((album) =>
+      album.id === dto.id ? { ...album, ...dto } : album,
+    );
+  }
+
+  deleteAlbum(id: string) {
+    return (this.db.albums = this.db.albums.filter((album) => album.id !== id));
+  }
+
+  // Favorites
+  getAllFavorites(): Fav {
+    return this.db.favorites;
+  }
+
+  addTrackToFavorites(id: string) {
+    return this.db.favorites.tracks.push(id);
+  }
+
+  isTrackInFavorites(trackId: string): boolean {
+    return this.db.favorites.tracks.includes(trackId);
+  }
+
+  deleteTrackFromFavorites(id: string) {
+    this.db.favorites.tracks = this.db.favorites.tracks.filter(
+      (trackId) => trackId !== id,
+    );
+  }
+
+  addAlbumToFavorites(id: string) {
+    return this.db.favorites.albums.push(id);
+  }
+
+  isAlbumInFavorites(albumId: string): boolean {
+    return this.db.favorites.albums.includes(albumId);
+  }
+
+  deleteAlbumFromFavorites(id: string) {
+    return (this.db.favorites.albums = this.db.favorites.albums.filter(
+      (albumId) => albumId !== id,
+    ));
+
+    // console.log(this.db.favorites);
+    // this.db.favorites.albums.push("roman")
+
+    // console.log(this.db.favorites);
+  }
+
+  addArtistToFavorites(id: string) {
+    return this.db.favorites.artists.push(id);
+  }
+
+  isArtistInFavorites(artistId: string): boolean {
+    return this.db.favorites.artists.includes(artistId);
+  }
+
+  deleteArtistFromFavorites(id: string) {
+    this.db.favorites.artists = this.db.favorites.artists.filter(
+      (artistId) => artistId !== id,
+    );
   }
 }
