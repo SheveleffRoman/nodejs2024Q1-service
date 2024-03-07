9868ad905fd53e54d3d3bc28ef079c65f0f51d6c
SheveleffRoman <@>
2024-03-08 01:07:55 +0400

    feat: add imports to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DbService } from './database/db-service';
 import { TrackModule } from './track/track.module';
+import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [UserModule, TrackModule],
+  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
   controllers: [AppController],
   providers: [AppService, DbService],
 })
