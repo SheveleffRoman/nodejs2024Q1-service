fd0f9a6f2c90d3f1ed87d2deeba552abfaf353b9
SheveleffRoman <@>
2024-03-11 17:40:21 +0400

    refactor: artist service methods short & clear

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,7 +1,7 @@
-import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
-import { DbService } from 'src/database/db-service';
+import { DbService } from 'src/database/db.service';
 import { Artist } from './entities/artist.entity';
 import { v4 as uuid } from 'uuid';
 
@@ -9,14 +9,12 @@ import { v4 as uuid } from 'uuid';
 export class ArtistService {
   constructor(private dbService: DbService) {}
 
-  private readonly logger = new Logger(ArtistService.name);
-
   create(createArtistDto: CreateArtistDto) {
-    const artist: Artist = {
+    const newArtist: Artist = {
       id: uuid(),
       ...createArtistDto,
     };
-    return this.dbService.addArtist(artist);
+    return this.dbService.addArtist(newArtist);
   }
 
   findAll() {
@@ -24,72 +22,42 @@ export class ArtistService {
   }
 
   async findOne(id: string) {
-    const artist = this.dbService.getArtistById(id);
-    if (!artist)
-      throw new HttpException("Artist doesn't exist", HttpStatus.NOT_FOUND);
+    const artist = await this.dbService.getArtistById(id);
+    if (!artist) throw new NotFoundException();
     return artist;
   }
 
   async update(id: string, updateArtistDto: UpdateArtistDto) {
     const artist = await this.findOne(id);
-    if (!artist)
-      throw new HttpException("Artist doesn't exist", HttpStatus.NOT_FOUND);
 
-    if (!updateArtistDto) {
-      throw new HttpException('Invalid dto format', HttpStatus.BAD_REQUEST);
-    }
-
-    const updatedArtist = {
+    const updatedArtist: Artist = {
       ...artist,
       ...updateArtistDto,
     };
+
     this.dbService.updateArtist(updatedArtist);
+
     return updatedArtist;
   }
 
   async remove(id: string) {
     const artist = await this.findOne(id);
-    if (!artist)
-      throw new HttpException("Artist doesn't exist", HttpStatus.NOT_FOUND);
     this.dbService.deleteArtist(id);
 
-    // change artist id to null in all tracks
-    const tracks = this.dbService.getAllTracks();
-    const filteredTracks = tracks.filter((track) => track.artistId === id);
-    filteredTracks.forEach((track) => {
-      this.dbService.updateTrack({ ...track, artistId: null });
-    });
+    const tracks = await this.dbService.getAllTracks();
+    const correspondingTracks = tracks.filter((track) => track.artistId === id);
+    correspondingTracks.map((track) =>
+      this.dbService.updateTrack({ ...track, artistId: null }),
+    );
+
+    const albums = await this.dbService.getAllAlbums();
+    const correspondingAlbums = albums.filter((album) => album.artistId === id);
+    correspondingAlbums.map((album) =>
+      this.dbService.updateAlbum({ ...album, artistId: null }),
+    );
 
     this.dbService.deleteArtistFromFavorites(id);
 
     return artist;
-
-    // // change artist id to null in all tracks
-    // const tracks = this.dbService.getAllTracks();
-    // this.logger.log(tracks[0]);
-    // const filteredTracks = tracks.filter((track) => track.artistId === id);
-    // this.logger.log(filteredTracks[0]);
-    // filteredTracks.forEach(async (track) => {
-    //   const updatedtrack = {
-    //     ...track,
-    //     artistId: null,
-    //   };
-    //   this.dbService.updateTrack(updatedtrack);
-    //   this.logger.log(updatedtrack);
-    // });
   }
 }
-// const tracks = this.dbService.getAllTracks();
-// const filteredTracks = tracks.filter((track) => track.artistId === id);
-// filteredTracks.forEach((track) => {
-//   this.dbService.updateTrack({ ...track, artistId: null });
-// });
-
-// change album id to null in all tracks
-// const albums = await this.dbService.getAllAlbums();
-// const filteredAlbums = albums.filter((album) => album.artistId === id);
-// filteredAlbums.forEach((album) => {
-//   this.dbService.updateAlbum({ ...album, artistId: null });
-// });
-
-// this.dbService.deleteArtistFromFavorites(id);
